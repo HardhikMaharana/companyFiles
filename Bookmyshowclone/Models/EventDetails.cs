2abe17bf3adc8c5a7aad676c9c111301bce275a1
HardhikMaharana <55658747+HardhikMaharana@users.noreply.github.com>
2023-11-30 18:52:16 +0530

    Add files via upload

---
+++ Bookmyshowclone/Models/EventDetails.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ManagementWebApp.Models
+{
+    public class EventDetails
+    {
+        public int id { get; set; }
+        public string EventName { get; set; }
+        public string Description { get; set; }
+        public string EventType { get; set; }
+        public string Duration { get; set; }
+        public Nullable<System.DateTime> Date { get; set; }
+        public string Language { get; set; }
+        public string CasteName { get; set; }
+        public byte[] Image { get; set; }
+        public string CreatedBy { get; set; }
+        public Nullable<System.DateTime> CreatedOn { get; set; }
+        public string UpdatedBy { get; set; }
+        public Nullable<System.DateTime> UpdatedOn { get; set; }
+        public Nullable<bool> IsActive { get; set; }
+    }
+}
